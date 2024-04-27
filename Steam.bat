b2e06627eb09ea8c606e5f7c7c9d6092656e9210
mtytyx <168254237+mtytyx@users.noreply.github.com>
2024-04-27 01:41:20 -0300

    Steam.bat

---
+++ Steam.bat
@@ -0,0 +1,1 @@
+start "" "C:\Program Files (x86)\SteamEX\steam.exe" -silent -vgui -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-d3d11 -cef-disable-sandbox -disable-winh264 -no-cef-sandbox -vrdisable -cef-disable-breakpad +open steam://open/minigameslist
