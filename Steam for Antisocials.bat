0d15ae01160cf4f267c8ebabec68dba15a50f4e8
mtytyx <168254237+mtytyx@users.noreply.github.com>
2024-04-27 02:18:15 -0300

    Steam for Antisocials.bat

--- Steam for Antisocials.bat
+++ Steam for Antisocials.bat
@@ -1,1 +1,1 @@
-
+start "" "C:\Program Files (x86)\SteamEX\steam.exe" -silent -vgui -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-d3d11 -cef-disable-sandbox -disable-winh264 -cef-force-32bit -no-cef-sandbox -vrdisable -cef-disable-breakpad +open steam://open/minigameslist
