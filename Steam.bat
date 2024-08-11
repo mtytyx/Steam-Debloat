ae5e1ddaecf95bcffb4d7115bbf541e132006ff0
mtytyx <168254237+mtytyx@users.noreply.github.com>
2024-08-10 22:54:40 -0300

    Update Steam.bat

--- Steam.bat
+++ Steam.bat
@@ -1,1 +1,34 @@
-start "" "C:\Program Files (x86)\Steam\steam.exe" -silent -vgui -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-d3d11 -cef-disable-sandbox -disable-winh264 -no-cef-sandbox -vrdisable -cef-disable-breakpad +open steam://open/minigameslist
+start "" "C:\Program Files (x86)\Steam\steam.exe" ^
+    -silent ^
+    -vgui ^
+    -nofriendsui ^
+    -no-dwrite ^
+    -nointro ^
+    -nobigpicture ^
+    -nofasthtml ^
+    -nocrashmonitor ^
+    -noshaders ^
+    -no-shared-textures ^
+    -disablehighdpi ^
+    -cef-single-process ^
+    -cef-in-process-gpu ^
+    -single_core ^
+    -cef-disable-d3d11 ^
+    -cef-disable-sandbox ^
+    -disable-winh264 ^
+    -no-cef-sandbox ^
+    -vrdisable ^
+    -cef-disable-breakpad ^
+    -no-browser ^
+    -disable-animations ^
+    -disable-ui ^
+    -disable-voice ^
+    -disable-microphone ^
+    -disable-overlay ^
+    -disable-sound ^
+    -no-boot ^
+    -no-restore ^
+    -no-multithread ^
+    -no-remote ^
+    -no-repair ^
+    +open steam://open/minigameslist
