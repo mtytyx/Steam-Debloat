23fbb0bcab08601f7972667185228ac23fd2551f
mtytyx <168254237+mtytyx@users.noreply.github.com>
2024-09-08 13:19:03 -0300

    Delete Steam.bat

--- Steam.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" /B /LOW "C:\Program Files (x86)\Steam\steam.exe" -silent -nofriendsui -nointro -nocrashmonitor -tcp -disablehighdpi -disable-voice -disable-overlay -lowpower -no-dwrite -no-verifyfiles -noforwarding -no-background-updates
