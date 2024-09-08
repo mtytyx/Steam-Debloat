9e51d8d5d0edda063d43a55422dec80aed702f6b
mtytyx <168254237+mtytyx@users.noreply.github.com>
2024-09-08 17:15:13 -0300

    Update Steam.bat

--- script/Steam.bat
+++ script/Steam.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" /B /LOW "C:\Program Files (x86)\Steam\steam.exe" -silent -nofriendsui -nointro -nocrashmonitor -tcp -disablehighdpi -disable-voice -disable-overlay -lowpower -no-dwrite -no-verifyfiles -noforwarding -no-background-updates
+start "" /B /LOW "C:\Program Files (x86)\Steam\steam.exe" -silent -nointro -single_core -nocrashmonitor -tcp -disablehighdpi -lowpower -no-dwrite -no-verifyfiles -noforwarding -skipstreaming -no-background-updates -cef-disable-remote-fonts -cef-disable-web-security -no-cafs -disable-broadcast -disable-steamvr -no-shaders -no-preload -no-splash -no-browser_throttle -no-sound -no-movies
