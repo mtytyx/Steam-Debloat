d8fb77fa024e2d64b7ee650216b3a85851c2643a
mtytyx <168254237+mtytyx@users.noreply.github.com>
2024-09-08 13:17:53 -0300

    Create Steam-lite.bat

---
+++ script/Steam-lite.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" /B /LOW "C:\Program Files (x86)\Steam\steam.exe" -silent -nofriendsui -nointro -nocrashmonitor -disablehighdpi -single_core -vrdisable -disable-voice -disable-overlay -tcp -cef-force-32bit -no-dwrite -no-verifyfiles -skipstreaming -skiplogin -no-background-updates -lowpower -windowed
