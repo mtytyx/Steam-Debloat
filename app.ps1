a71c5ac8439488edd6b0b455dc495372e5ce4ace
mtytyx <168254237+mtytyx@users.noreply.github.com>
2024-09-08 13:12:17 -0300

    Update app.ps1

--- app.ps1
+++ app.ps1
@@ -2,8 +2,7 @@ param (
     [string]$Mode = "Normal"
 )
 
-# Variables
-$title = "Steam Downgrade"
+$title = "Steam Optimization"
 $githubRepo = "GitHub.com/mtytyx"
 $color = "Green"
 
@@ -18,15 +17,10 @@ $urls = @{
     }
 }
 
-$fileSteamBat = "Steam.bat"
+$fileSteamBat = if ($Mode -eq "Lite") { "Steam-Lite.bat" } else { "Steam.bat" }
 $fileSteamCfg = "steam.cfg"
 $tempPath = $env:TEMP
 $steamPath = "C:\Program Files (x86)\Steam\steam.exe"
-
-if (-not $urls.ContainsKey($Mode)) {
-    Write-Host "[ERROR] Invalid mode specified. Use 'Normal' or 'Lite'."
-    Exit 1
-}
 
 $urlSteamBat = $urls[$Mode]["SteamBat"]
 $urlSteamCfg = $urls[$Mode]["SteamCfg"]
@@ -36,14 +30,14 @@ function Main {
     Kill-SteamProcesses
     Download-Files
     Start-Steam
-    Check-SteamClosed
+    Wait-For-SteamClosure
     Move-ConfigFile
-    Main-Menu
+    Finish
 }
 
 function Set-ConsoleProperties {
     $host.UI.RawUI.WindowTitle = "$title - $githubRepo"
-    Write-Host -ForegroundColor $color
+    Write-Host "[INFO] Starting $title in $Mode mode" -ForegroundColor $color
 }
 
 function Kill-SteamProcesses {
@@ -51,50 +45,29 @@ function Kill-SteamProcesses {
 }
 
 function Download-Files {
-    Write-Host "[INFO] Downloading files, please wait..."
     Invoke-WebRequest -Uri $urlSteamBat -OutFile "$tempPath\$fileSteamBat"
     Invoke-WebRequest -Uri $urlSteamCfg -OutFile "$tempPath\$fileSteamCfg"
-    Write-Host "[SUCCESS] Files downloaded successfully."
-    Start-Sleep -Seconds 2
 }
 
 function Start-Steam {
-    Write-Host "[INFO] Starting Steam for update..."
-    Start-Process -FilePath $steamPath -ArgumentList "-forcesteamupdate -forcepackagedownload -overridepackageurl https://archive.org/download/dec2022steam -exitsteam"
+    Write-Host "[INFO] Starting Steam for updates..."
+    Start-Process -FilePath $steamPath -ArgumentList "-forcesteamupdate -exitsteam"
     Start-Sleep -Seconds 5
 }
 
-function Check-SteamClosed {
-    Write-Host "[INFO] Waiting for Steam to close completely..."
-    while (Get-Process -Name "steam", "steamservice", "steamwebhelper" -ErrorAction SilentlyContinue) {
-        Start-Sleep -Seconds 10
+function Wait-For-SteamClosure {
+    Write-Host "[INFO] Waiting for Steam to close..."
+    while (Get-Process -Name "steam" -ErrorAction SilentlyContinue) {
+        Start-Sleep -Seconds 5
     }
 }
 
 function Move-ConfigFile {
-    Write-Host "[INFO] Moving configuration file..."
     Move-Item -Path "$tempPath\$fileSteamCfg" -Destination "C:\Program Files (x86)\Steam\steam.cfg" -Force
-    Write-Host "[SUCCESS] Configuration file moved successfully."
 }
 
-function Main-Menu {
-    Clear-Host
-    Write-Host "[INFO] Process completed. Do you want to move the .bat file to the desktop?"
-    Write-Host "1) Yes"
-    Write-Host "2) No"
-    $option = Read-Host "Select an option (1 or 2)"
-    if ($option -eq "1") {
-        Move-ToDesktop
-    } elseif ($option -eq "2") {
-        Exit
-    }
-}
-
-function Move-ToDesktop {
-    Write-Host "[INFO] Moving .bat file to the desktop..."
-    Move-Item -Path "$tempPath\$fileSteamBat" -Destination "C:\Users\$env:USERNAME\Desktop" -Force
-    Start-Sleep -Seconds 3
-    Exit
+function Finish {
+    Write-Host "[SUCCESS] Steam configured and updated."
 }
 
 Main
