7cb0e2526018fbe33cf5d3948d98c969ba8cdaca
mtytyx <168254237+mtytyx@users.noreply.github.com>
2024-09-08 13:17:34 -0300

    Create Steam.bat

---
+++ script/Steam.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" /B /LOW "C:\Program Files (x86)\Steam\steam.exe" -silent -nofriendsui -nointro -nocrashmonitor -tcp -disablehighdpi -disable-voice -disable-overlay -lowpower -no-dwrite -no-verifyfiles -noforwarding -no-background-updates
