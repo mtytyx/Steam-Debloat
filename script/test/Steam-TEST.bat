c212da014ca3a87d4dac9b79ac41331f6cad27d4
kid <168254237+mtytyx@users.noreply.github.com>
2024-09-09 18:14:17 -0300

    Update Steam-TEST.bat

--- script/test/Steam-TEST.bat
+++ script/test/Steam-TEST.bat
@@ -1,1 +1,2 @@
-
+@echo off
+start "" /B /LOW "C:\Program Files (x86)\Steam\steam.exe" -silent -nointro -single_core -nocrashmonitor -tcp -disablehighdpi -lowpower -no-dwrite -no-verifyfiles -noforwarding -skipstreaming -no-background-updates -cef-disable-remote-fonts -cef-disable-web-security -no-cafs -disable-broadcast -disable-steamvr -no-shaders -no-preload -no-splash -no-browser_throttle -no-sound -no-movies
