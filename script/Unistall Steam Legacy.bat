070d39646fc57cb2ba1ebb6084af1a502b9c5574
kid <168254237+mtytyx@users.noreply.github.com>
2024-09-09 11:51:07 -0300

    Update Unistall Steam Legacy.bat

--- script/Unistall Steam Legacy.bat
+++ script/Unistall Steam Legacy.bat
@@ -1,1 +1,5 @@
-
+@echo off
+taskkill /F /IM steam.exe >nul 2>&1
+del "%ProgramFiles(x86)%\Steam\package\packageinfo.vdf" >nul 2>&1
+start "" "%ProgramFiles(x86)%\Steam\Steam.exe" -forcesteamupdate -forcepackagedownload -overridepackageurl http://web.archive.org/web/20240717082107if_/media.steampowered.com/client -exitsteam
+exit
