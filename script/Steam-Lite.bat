541313ff7e540ab2db286e41bf4fbe2c23d74007
kid <168254237+mtytyx@users.noreply.github.com>
2024-09-17 14:01:17 -0300

    Update Steam-Lite.bat

--- script/Steam-Lite.bat
+++ script/Steam-Lite.bat
@@ -4,7 +4,7 @@
 
 set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
 
-set "OPTS=-silent -no-browser -nobootstrapupdate -nobigpicture -noverifyfiles -norepairfiles -noshaders -no-dwrite -disable-gpu -nocrashdialog -single_core -tcp -clearbeta -netbuffer 131072 -cef-single-process -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning -cef-disable-remote-fonts"
+set "OPTS=-silent -nobootstrapupdate -nobigpicture -noverifyfiles -norepairfiles -noshaders -no-dwrite -disable-gpu -nocrashdialog -single_core -tcp -clearbeta -netbuffer 131072 -cef-single-process -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning -cef-disable-remote-fonts"
 
 start "" /B /LOW "%STEAM_PATH%" %OPTS%
 exit
