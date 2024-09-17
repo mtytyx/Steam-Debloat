c948dbc7690c66e5c387fa69e21e1fafe60e0fe4
kid <168254237+mtytyx@users.noreply.github.com>
2024-09-17 13:55:29 -0300

    Update Steam.bat

--- script/Steam.bat
+++ script/Steam.bat
@@ -4,7 +4,7 @@
 
 set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
 
-set "OPTS=-silent -noverifyfiles -norepairfiles -noshaders -no-dwrite -nocrashdialog -single_core -tcp -clearbeta -netbuffer 262144 -cef-single-process -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning -cef-disable-remote-fonts -no-browser -disable-gpu-vsync -threads 2"
+set "OPTS=-silent -noverifyfiles -norepairfiles -noshaders -no-dwrite -nocrashdialog -single_core -tcp -clearbeta -netbuffer 262144 -cef-single-process -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning -cef-disable-remote-fonts -disable-gpu-vsync -threads 2"
 
 start "" /B /NORMAL "%STEAM_PATH%" %OPTS%
 exit
