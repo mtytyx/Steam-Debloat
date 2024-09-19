25768c650ceba5b476cafa0f8d115e21b3799d84
kid <168254237+mtytyx@users.noreply.github.com>
2024-09-19 19:37:57 -0300

    Update Steam-TEST.bat

--- script/test/Steam-TEST.bat
+++ script/test/Steam-TEST.bat
@@ -7,19 +7,19 @@ setlocal enabledelayedexpansion
 set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
 
 :: Base options
-set "BASE_OPTS=-silent -noverifyfiles -norepairfiles -noshaders -no-dwrite -nocrashdialog -single_core -tcp -clearbeta"
+set "BASE_OPTS=-silent -noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta"
 
 :: Toggleable options (0 = off, 1 = on)
-set "DISABLE_BROWSER=1"
-set "DISABLE_GPU=1"
+set "DISABLE_BROWSER=0"
+set "DISABLE_GPU=0"
 set "USE_CEF_SINGLE_PROCESS=1"
 set "DISABLE_VR=1"
 set "LIMIT_THREADS=1"
 
 :: Build options string
 set "OPTS=!BASE_OPTS!"
-if %DISABLE_BROWSER%==1 set "OPTS=!OPTS! -cef-disable-remote-fonts"
-if %DISABLE_GPU%==1 set "OPTS=!OPTS! -disable-gpu -disable-gpu-vsync"
+if %DISABLE_BROWSER%==1 set "OPTS=!OPTS! -no-browser"
+if %DISABLE_GPU%==1 set "OPTS=!OPTS! -disable-gpu-vsync"
 if %USE_CEF_SINGLE_PROCESS%==1 set "OPTS=!OPTS! -cef-single-process -no-cef-sandbox"
 if %DISABLE_VR%==1 set "OPTS=!OPTS! -vrdisable"
 if %LIMIT_THREADS%==1 set "OPTS=!OPTS! -threads 2"
