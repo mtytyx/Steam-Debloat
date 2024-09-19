eb319a087dd131f0f539c78bd16a7532a22f5939
kid <168254237+mtytyx@users.noreply.github.com>
2024-09-19 19:40:11 -0300

    Update Steam-Lite-TEST.bat

--- script/test/Steam-Lite-TEST.bat
+++ script/test/Steam-Lite-TEST.bat
@@ -4,7 +4,7 @@
 
 set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
 
-set "OPTS=-silent -nobootstrapupdate -nobigpicture -noverifyfiles -norepairfiles -noshaders -no-dwrite -disable-gpu -disable-d3d9ex -nocrashdialog -nofriendsui -single_core -tcp -clearbeta -netbuffer 131072 -cef-single-process -cef-in-process-gpu -no-cef-sandbox -disable-winh264 -cef-disable-d3d11 -vrdisable"
+set "OPTS=-silent -nobootstrapupdate -nobigpicture -noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta -netbuffer 131072 -cef-single-process -no-cef-sandbox -vrdisable"
 
-start "" /B /LOW /REALTIME "%STEAM_PATH%" %OPTS%
+start "" /B /LOW "%STEAM_PATH%" %OPTS%
 exit
