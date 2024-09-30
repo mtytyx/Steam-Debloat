f434ba5a466b9103985e0615ad40e6d98115275d
kid <168254237+mtytyx@users.noreply.github.com>
2024-09-30 16:00:48 -0300

    Update Steam.bat

--- script/Steam.bat
+++ script/Steam.bat
@@ -5,7 +5,7 @@
 set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
 
 :: Options adjusted to enable minimal GPU acceleration and resolve black library screen issue
-set "OPTS=-silent -noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta -netbuffer 262144 -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning -threads 2"
+set "OPTS=-noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta -netbuffer 262144 -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning -threads 2"
 
 start "" /B /NORMAL "%STEAM_PATH%" %OPTS%
 exit
