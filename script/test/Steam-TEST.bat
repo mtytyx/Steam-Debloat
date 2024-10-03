6c18248d558c49c7bbbd68ebcfdbe4ba96c1966d
kid <168254237+mtytyx@users.noreply.github.com>
2024-10-03 17:33:36 -0300

    Update Steam-TEST.bat

--- script/test/Steam-TEST.bat
+++ script/test/Steam-TEST.bat
@@ -1,31 +1,10 @@
 @echo off
-setlocal enabledelayedexpansion
-
-:: Steam Test Launcher
-:: Configurable options for testing different optimization levels
+:: Steam Lite Launcher (Test Mode)
+:: Designed for absolute minimal resource usage and extensive testing
 
 set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
 
-:: Base options
-set "BASE_OPTS=-noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta"
+set "OPTS=-nobootstrapupdate -nobigpicture -noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta -netbuffer 65536 -cef-single-process -no-cef-sandbox -vrdisable -no-browser -disable-gpu-vsync -disable-gpu -cef-force-32bit -no-shared-textures -disable-font-subpixel-positioning -disable-smooth-scrolling -threads 1 -offline -silent -console"
 
-:: Toggleable options (0 = off, 1 = on)
-set "DISABLE_BROWSER=0"
-set "DISABLE_GPU=0"
-set "USE_CEF_SINGLE_PROCESS=1"
-set "DISABLE_VR=1"
-set "LIMIT_THREADS=1"
-
-:: Build options string
-set "OPTS=!BASE_OPTS!"
-if %DISABLE_BROWSER%==1 set "OPTS=!OPTS! -no-browser"
-if %DISABLE_GPU%==1 set "OPTS=!OPTS! -disable-gpu-vsync"
-if %USE_CEF_SINGLE_PROCESS%==1 set "OPTS=!OPTS! -cef-single-process -no-cef-sandbox"
-if %DISABLE_VR%==1 set "OPTS=!OPTS! -vrdisable"
-if %LIMIT_THREADS%==1 set "OPTS=!OPTS! -threads 2"
-
-:: Additional optimizations
-set "OPTS=!OPTS! -netbuffer 262144 -no-shared-textures -disable-font-subpixel-positioning"
-
-start "" /B /LOW "%STEAM_PATH%" !OPTS!
+start "" /B /LOW "%STEAM_PATH%" %OPTS%
 exit
