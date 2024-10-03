83ddbd3c93925b729059341682b424aad249558f
kid <168254237+mtytyx@users.noreply.github.com>
2024-10-03 17:32:44 -0300

    Update Steam.bat

--- script/Steam.bat
+++ script/Steam.bat
@@ -1,11 +1,11 @@
 @echo off
-:: Steam Balanced Launcher
-:: Optimized for low resource usage while maintaining most functionality
+:: Steam Balanced Launcher (Normal Mode)
+:: Optimized for good performance while maintaining normal functionality
 
 set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
 
-:: Options adjusted to enable minimal GPU acceleration and resolve black library screen issue
-set "OPTS=-noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta -netbuffer 262144 -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning -threads 2"
+:: Options balanced for performance and functionality
+set "OPTS=-noverifyfiles -norepairfiles -noshaders -nocrashdialog -tcp -clearbeta -netbuffer 524288 -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning -threads 4 -console"
 
 start "" /B /NORMAL "%STEAM_PATH%" %OPTS%
 exit
