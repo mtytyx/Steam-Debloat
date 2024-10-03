3e28a539c14070f31490bae7b5ee65515b2494c8
kid <168254237+mtytyx@users.noreply.github.com>
2024-10-03 17:33:05 -0300

    Update Steam-Lite.bat

--- script/Steam-Lite.bat
+++ script/Steam-Lite.bat
@@ -1,11 +1,10 @@
-# Steam-Lite.bat
 @echo off
 :: Steam Lite Launcher
-:: Balanced approach for low resource usage while maintaining basic functionality
+:: Optimized for lower resource usage while maintaining essential functionality
 
 set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
 
-set "OPTS=-nobootstrapupdate -nobigpicture -noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta -netbuffer 131072 -cef-single-process -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning"
+set "OPTS=-nobootstrapupdate -nobigpicture -noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta -netbuffer 262144 -cef-single-process -no-cef-sandbox -vrdisable -no-shared-textures -disable-font-subpixel-positioning -cef-force-32bit -disable-gpu-vsync -console"
 
 start "" /B /LOW "%STEAM_PATH%" %OPTS%
 exit
