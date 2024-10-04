f81391bf6881817335aa0b563e4b0cfe493bf2d5
kid <168254237+mtytyx@users.noreply.github.com>
2024-10-04 15:36:55 -0300

    update -no-browser

--- script/test/Steam-Lite-TEST.bat
+++ script/test/Steam-Lite-TEST.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "C:\Program Files (x86)\Steam"
-start steam.exe -silent -cef-force-32bit -no-browser -no-dwrite -no-cef-sandbox -nooverlay -nofriendsui -nobigpicture -noshaders -novid -noverifyfiles -nointro -skipstreamingdrivers -norepairfiles -nohltv -480p -cef-disable-gpu -cef-disable-hang-timeouts -cef-disable-seccomp-sandbox -cef-disable-gpu-compositing -console
+start steam.exe -silent -cef-force-32bit -no-dwrite -no-cef-sandbox -nooverlay -nofriendsui -nobigpicture -noshaders -novid -noverifyfiles -nointro -skipstreamingdrivers -norepairfiles -nohltv -480p -cef-disable-gpu -cef-disable-hang-timeouts -cef-disable-seccomp-sandbox -cef-disable-gpu-compositing -console
