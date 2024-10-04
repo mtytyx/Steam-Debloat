dc0e73fb75b7157d1409b4f017d3125ff7f09abf
kid <168254237+mtytyx@users.noreply.github.com>
2024-10-04 14:20:02 -0300

    Update Steam-TEST.bat

--- script/test/Steam-TEST.bat
+++ script/test/Steam-TEST.bat
@@ -1,10 +1,3 @@
 @echo off
-:: Steam Lite Launcher (Test Mode)
-:: Designed for absolute minimal resource usage and extensive testing
-
-set "STEAM_PATH=C:\Program Files (x86)\Steam\steam.exe"
-
-set "OPTS=-nobootstrapupdate -nobigpicture -noverifyfiles -norepairfiles -noshaders -nocrashdialog -single_core -tcp -clearbeta -netbuffer 65536 -cef-single-process -no-cef-sandbox -vrdisable -no-browser -disable-gpu-vsync -disable-gpu -cef-force-32bit -no-shared-textures -disable-font-subpixel-positioning -disable-smooth-scrolling -threads 1 -offline -silent -console"
-
-start "" /B /LOW "%STEAM_PATH%" %OPTS%
-exit
+cd /d "C:\Program Files (x86)\Steam"
+start steam.exe -silent -cef-force-32bit -no-browser -no-dwrite -no-cef-sandbox -nooverlay -nofriendsui -nobigpicture -noshaders -novid -noverifyfiles -nointro -skipstreamingdrivers -norepairfiles -nohltv -720p -cef-disable-gpu -cef-disable-hang-timeouts -cef-disable-seccomp-sandbox -console
