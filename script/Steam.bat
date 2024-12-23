27569e0838fd95bb086b545cc8d2b41748ca4a8d
kid <168254237+mtytyx@users.noreply.github.com>
2024-12-22 21:56:26 -0300

    Delete script/Steam.bat

--- script/Steam.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Program Files (x86)\Steam"
-start steam.exe -silent -no-dwrite -no-cef-sandbox -nooverlay -nobigpicture -nofriendsui -noshaders -novid -noverifyfiles -nointro -skipstreamingdrivers -norepairfiles -nohltv -720p -cef-disable-gpu -cef-disable-hang-timeouts -cef-disable-seccomp-sandbox -cef-disable-extensions -cef-disable-remote-fonts -cef-enable-media-stream -cef-disable-accelerated-video-decode
