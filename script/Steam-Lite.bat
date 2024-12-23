6f35924b2d9a8df0d1eec400e1a694feb2724ae8
kid <168254237+mtytyx@users.noreply.github.com>
2024-12-22 21:56:34 -0300

    Delete script/Steam-Lite.bat

--- script/Steam-Lite.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Program Files (x86)\Steam"
-start steam.exe -silent -cef-force-32bit -no-dwrite -no-cef-sandbox -nooverlay -nofriendsui -nobigpicture -noshaders -novid -noverifyfiles -nointro -skipstreamingdrivers -norepairfiles -nohltv -480p -cef-disable-gpu -cef-disable-hang-timeouts -cef-disable-seccomp-sandbox -cef-disable-gpu-compositing -cef-disable-extensions -cef-disable-remote-fonts -cef-enable-media-stream -cef-disable-accelerated-video-decode
